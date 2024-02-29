3e8f35ddd8c9c9e41d808063d873707615763413
Erko Abdurahman <erkoabdu@gmail.com>
2024-02-29 16:20:28 -0500

    main logic complete

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-let btn_no = document.querySelector("btn__no");
-let screenWidth;
-let screenHeight;
+window.onload = function () {
+  let btn_no = document.querySelector(".btn__no");
+  let btn_yes = document.querySelector(".btn__yes");
+  let header = document.querySelector(".header");
+  let mainHeader = document.querySelector(".shrek__main_header");
+  let mainImage = document.querySelector(".shrek__main_image");
+  let yesImage = document.querySelector(".shrek__yes_image");
+  let timeoutHeader = document.querySelector(".shrek__no_timeout");
+  let timeoutImage = document.querySelector(".shrek__no_image");
+  let fiveMinImage = document.querySelector(".shrek__no_image");
 
-function initNoBtn() {
-  spaceWidth = document.body.offsetHeight - parseInt(btn_no.style.hieght);
-  spaceHeight = document.body.offsetWidth - parseInt(btn_no.style.width);
-}
+  let spaceWidth;
+  let spaceHeight;
+
+  function wasteTime() {
+    header.style.display = "none";
+    mainHeader.style.display = "none";
+    mainImage.style.display = "none";
+    timeoutHeader.style.display = "block";
+    timeoutImage.style.display = "block";
+    btn_yes.style.display = "none";
+    btn_no.style.display = "none";
+  }
+  function clickYes() {
+    header.style.display = "none";
+    mainHeader.style.display = "block";
+    mainImage.style.display = "none";
+    yesImage.style.display = "block";
+    btn_yes.style.display = "none";
+    btn_no.style.display = "none";
+  }
+  function moveBtn() {
+    btn_no.style.top = Math.round(Math.random() * spaceWidth) + "px";
+    btn_no.style.left = Math.round(Math.random() * spaceHeight) + "px";
+  }
+  function initBtn() {
+    spaceWidth = window.innerHeight - btn_no.offsetHeight;
+    spaceHeight = window.innerWidth - btn_no.offsetWidth;
+
+    btn_no.addEventListener("mouseover", moveBtn);
+  }
+
+  //setTimeout(wasteTime, 300000);
+  btn_yes.addEventListener("click", clickYes);
+  initBtn();
+};
